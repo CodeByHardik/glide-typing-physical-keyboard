8260e34d305f6bfc53938c5b83caa7adcb79f844
Hardik Dubey <titan.d3athopp@gmail.com>
2025-03-19 22:02:12 +0530

    Initialized

---
+++ src/key_tracker.cpp
@@ -0,0 +1,11 @@
+#include <linux/input.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Glide Typing Tracker Initialized!" << endl;
+    return 0;
+}
